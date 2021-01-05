d9e77fb92873e5407c79986773dfe41aa3c4225d
seokho10007 <sukho1007@naver.com>
2021-01-06 03:37:18 +0900

    [Feat] 컴포넌트 설정 및 구조 작성

---
+++ front/src/pages/_app.jsx
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import React, { Fragment } from 'react';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+import AppLayOut from '../components/AppLayOut';
+import theme from '../theme';
+
+const GlobalStyle = createGlobalStyle`
+	${reset}
+	a,
+	ul,
+	li,
+	button{
+		 border: 0;
+         text-decoration:none;
+         color:inherit;
+		 outline:none;
+		 list-style:none;
+		 text-transform: none;
+     }
+	 
+     *{
+         box-sizing: border-box;
+		 margin:0;
+		 padding:0;
+     }
+     html,
+	 body{
+		 margin:0;
+		 padding:0;
+		 width:100%;
+		 height:100%;
+         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+         font-size: 10px;
+		 background-color: #EFF2F5;
+     }
+`;
+
+const SH_BLOG = ({ Component, pageProps }) => {
+	return (
+		<Fragment>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Head>
+					<title>SH BLOG</title>
+				</Head>
+				<AppLayOut>
+					<Component {...pageProps} />
+				</AppLayOut>
+			</ThemeProvider>
+		</Fragment>
+	);
+};
+
+export default SH_BLOG;
