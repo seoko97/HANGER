12605756c055795c3e4eb064de3ac29b7c39bfc8
seokho10007 <sukho1007@naver.com>
2021-01-08 04:42:15 +0900

    [chore] 패키지 및 설정 추가

---
+++ front/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	trailingSlash: true,
+	// exportPathMap: async function (
+	// 	defaultPathMap,
+	// 	{ dev, dir, outDir, distDir, buildId },
+	// ) {
+	// 	return {
+	// 		'/': { page: '/index' },
+	// 	};
+	// },
+};
