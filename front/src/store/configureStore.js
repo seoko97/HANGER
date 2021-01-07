1fdb1f6f61488182e6dbe74854273ff5a8fac8f3
seokho10007 <sukho1007@naver.com>
2021-01-08 04:51:13 +0900

    [feat] DevtoolsMiddleware 생성 및 store 추가

---
+++ front/src/store/configureStore.js
@@ -0,0 +1,27 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createWrapper } from 'next-redux-wrapper';
+
+import reducer from '../reducers';
+import rootSaga from '../sagas';
+
+const configureStore = () => {
+	const sagaMiddleware = createSagaMiddleware();
+	const middleware = [sagaMiddleware];
+
+	const enhancer =
+		process.env.NODE_ENV === 'production'
+			? compose(applyMiddleware(...middleware))
+			: composeWithDevTools(applyMiddleware(...middleware));
+
+	const store = createStore(reducer, enhancer);
+	store.sagaTask = sagaMiddleware.run(rootSaga);
+	return store;
+};
+
+const wrapper = createWrapper(configureStore, {
+	debug: process.env.NODE_ENV === 'development',
+});
+
+export default wrapper;
