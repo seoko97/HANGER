bdab2eb6c74aa94d5cc90691f276edcb08f9d28a
seokho10007 <sukho1007@naver.com>
2021-01-08 04:49:45 +0900

    [feat] rootReducer init

---
+++ front/src/reducers/index.js
@@ -0,0 +1,19 @@
+import { HYDRATE } from 'next-redux-wrapper';
+import { combineReducers } from 'redux';
+import user from './user';
+
+const rootReducer = (state, action) => {
+	switch (action.type) {
+		case HYDRATE:
+			return action.payload;
+
+		default: {
+			const combineReducer = combineReducers({
+				user,
+			});
+			return combineReducer(state, action);
+		}
+	}
+};
+
+export default rootReducer;
