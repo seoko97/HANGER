daf907cdefd739c6353eb1c0f3c69ca1e33ce8b5
seokho10007 <sukho1007@naver.com>
2021-01-08 04:50:06 +0900

    [feat] rootSaga init

---
+++ front/src/sagas/index.js
@@ -0,0 +1,11 @@
+import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+
+import userSaga from './user';
+
+axios.defaults.baseURL = 'http://localhost:3060';
+axios.defaults.withCredentials = true;
+
+export default function* rootSaga() {
+	yield all([fork(userSaga)]);
+}
