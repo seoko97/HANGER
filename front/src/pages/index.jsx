870e8f37a6ef7498a5a347a6f27ba8463f1bbfd6
seokho10007 <sukho1007@naver.com>
2021-01-12 01:25:00 +0900

    [feat] 사용자 정보 설정

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,13 +1,15 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
 
 const Home = () => {
 	const a = [1, 2];
+	const { me } = useSelector((state) => state.user);
 
 	return (
 		<Fragment>
-			<PostForm />
+			{me && <PostForm />}
 			<PostCard key="asdfasdfasdf" testPng="./testPng.png" />
 			{a.map((x, y) => (
 				<PostCard key={y} />
