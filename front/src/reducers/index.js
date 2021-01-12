7c33d543d71e0e034ef197871a2ed774691a61e5
seokho10007 <sukho1007@naver.com>
2021-01-13 02:45:26 +0900

    [feat] post reducer 추가

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
 import user from './user';
+import post from './post';
 
 const rootReducer = (state, action) => {
 	switch (action.type) {
@@ -10,6 +11,7 @@ const rootReducer = (state, action) => {
 		default: {
 			const combineReducer = combineReducers({
 				user,
+				post,
 			});
 			return combineReducer(state, action);
 		}
