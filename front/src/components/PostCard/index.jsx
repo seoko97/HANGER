ba111d65c7836303a83ce7c6385f96834d1005f3
seokho10007 <sukho1007@naver.com>
2021-01-13 02:47:38 +0900

    [style] Comment Form 생성 및 스타일 변경

--- front/src/components/PostCard/index.jsx
+++ front/src/components/PostCard/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
 import {
 	AiOutlineShareAlt,
 	AiOutlineMessage,
@@ -9,6 +10,10 @@ import {
 import PostImage from './PostImage';
 import Card from '../UI/Card';
 import Avatar from '../UI/Avatar';
+import Button from '../UI/Button';
+import useClick from '../../hooks/useClick';
+import useInput from '../../hooks/useInput';
+import { ADD_COMMENT_REQUEST } from '../../reducers/post';
 
 const PostCardWrapper = styled(Card)`
 	width: 90%;
@@ -78,6 +83,7 @@ const MetaDetailDescription = styled.div`
 	font-weight: normal;
 	box-sizing: border-box;
 	font-size: 1.4rem;
+	line-height: 1.3em;
 `;
 
 const PostCardAction = styled.ul`
@@ -98,41 +104,168 @@ const PostCardAction = styled.ul`
 	} */
 `;
 
-const PostCard = ({ testPng }) => {
+const CommentWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	border-top: 1px solid #e4e6eb;
+	flex-direction: column;
+	& > div {
+		width: 100%;
+		display: flex;
+	}
+`;
+
+const AddCommentFormWrapper = styled.form`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	padding: 1rem 2rem;
+
+	& > textarea {
+		font-size: 1.4rem;
+		height: auto;
+		border: none;
+		resize: none;
+		overflow: visible;
+		height: 18px;
+		outline: none;
+		flex-grow: 1;
+	}
+	& > button {
+		font-size: 1.4rem;
+		border: 0;
+		font-weight: 600;
+		box-shadow: 0;
+		color: #0095f6;
+		display: flex;
+		position: relative;
+		background-color: rgba(0, 0, 0, 0);
+		cursor: pointer;
+		&:hover {
+			color: #47b3f9;
+		}
+	}
+`;
+
+const CommentsList = styled.div`
+	flex: 1 1 auto;
+	padding: 1rem 2rem;
+	font-size: 1.4rem;
+
+	& > div:first-child {
+		font-size: 1.3rem;
+		font-weight: 300;
+	}
+
+	& > div {
+		display: block;
+		margin-bottom: 1.2rem;
+
+		& > span {
+			display: inline;
+			position: relative;
+			font-weight: 300;
+			line-height: 1.3em;
+		}
+
+		& > span:first-child {
+			margin-right: 0.6rem;
+			font-weight: 600;
+			cursor: pointer;
+		}
+	}
+`;
+
+const PostCard = ({ post }) => {
+	const dispatch = useDispatch();
+	const { me } = useSelector((state) => state.user);
+	const [commentForm, setCommentForm] = useClick(false);
+	const [comment, commentHandler, setComment] = useInput('');
+
+	const onSubmitComment = useCallback(
+		(e) => {
+			e.preventDefault();
+			dispatch({
+				type: ADD_COMMENT_REQUEST,
+				data: {
+					userId: me.id,
+					postId: post.id,
+					content: comment,
+				},
+			});
+			setComment('');
+		},
+		[me && me.id, comment],
+	);
+
 	return (
 		<PostCardWrapper>
 			<PostCardHead>
 				<button>팔로우</button>
 			</PostCardHead>
-			{testPng && <PostImage image={testPng} />}
+			{post.Images[0] && <PostImage images={post.Images[0]} />}
 			<PostCardBody>
 				<BodyMeta>
 					<div>
-						<Avatar>지</Avatar>
+						<Avatar>{post.User.nickname[0]}</Avatar>
 					</div>
 					<MetaDetail>
-						<MetaDetailTitle>지석호</MetaDetailTitle>
+						<MetaDetailTitle>{post.User.nickname}</MetaDetailTitle>
 						<MetaDetailDescription>
-							게시물 작성 게시물 작성 게시물 작성 게시물 작성
-							게시물 작성 게시물 작성
+							{post.content}
 						</MetaDetailDescription>
 					</MetaDetail>
 				</BodyMeta>
 			</PostCardBody>
 			<PostCardAction>
 				<li>
-					<AiOutlineShareAlt />
+					<AiOutlineShareAlt cursor="pointer" />
 				</li>
 				<li>
-					<AiOutlineHeart color="red" />
+					<AiOutlineHeart color="red" cursor="pointer" />
+				</li>
+				<li onClick={setCommentForm}>
+					<AiOutlineMessage cursor="pointer" />
 				</li>
 				<li>
-					<AiOutlineMessage />
-				</li>
-				<li>
-					<AiOutlineEllipsis />
+					<AiOutlineEllipsis cursor="pointer" />
 				</li>
 			</PostCardAction>
+			{commentForm && (
+				<CommentWrapper>
+					{me && (
+						<div>
+							<AddCommentFormWrapper onSubmit={onSubmitComment}>
+								{/* <textarea placeholder="댓글쓰기..." />
+							<Button>저장</Button> */}
+								<textarea
+									placeholder="댓글쓰기..."
+									value={comment}
+									onChange={commentHandler}
+								/>
+								<Button type="submit" defaultStyle={true}>
+									저장
+								</Button>
+							</AddCommentFormWrapper>
+						</div>
+					)}
+					<div>
+						<CommentsList>
+							<div>{post.Comments.length}개의 댓글</div>
+							{post.Comments.map((v) => (
+								<div key={v.User.id}>
+									<span>
+										<span>{v.User.nickname}</span>
+									</span>
+									<span>
+										<span>{v.content}</span>
+									</span>
+								</div>
+							))}
+						</CommentsList>
+					</div>
+				</CommentWrapper>
+			)}
 		</PostCardWrapper>
 	);
 };
