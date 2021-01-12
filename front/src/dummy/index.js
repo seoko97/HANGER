77246e9d207dc305ff142e120fa5f05dc1553f0b
seokho10007 <sukho1007@naver.com>
2021-01-13 02:45:02 +0900

    [feat] dummy post 생성

--- front/src/dummy/index.js
+++ front/src/dummy/index.js
@@ -1,3 +1,6 @@
+import shortId from 'shortid';
+import faker from 'faker';
+
 export const dummyUser1 = {
 	id: 0,
 	userId: 'wltjrgh',
@@ -8,3 +11,78 @@ export const dummyUser1 = {
 	gender: 'male',
 	posts: [],
 };
+
+export const dummyPost = (num) =>
+	Array(num)
+		.fill()
+		.map(() => ({
+			id: shortId.generate(),
+			User: {
+				id: shortId.generate(),
+				nickname: 'devlop_Ji',
+			},
+			content: faker.lorem.paragraph(),
+			Images: [
+				{
+					src: faker.image.image(),
+				},
+			],
+			Comments: [
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+				{
+					User: {
+						id: shortId.generate(),
+						nickname: faker.name.findName(),
+					},
+					content: faker.lorem.sentence(),
+				},
+			],
+		}));
