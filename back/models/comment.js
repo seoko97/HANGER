bf15f502a4f973d5331bd233510d8d52c374b7f3
seokho10007 <sukho1007@naver.com>
2021-01-15 04:26:17 +0900

    [feat] Comment 테이블 작성

---
+++ back/models/comment.js
@@ -0,0 +1,31 @@
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Comment extends (
+	Model
+) {
+	static init(sequelize) {
+		return super.init(
+			{
+				content: {
+					type: DataTypes.TEXT,
+					allowNull: false,
+				},
+			},
+			{
+				modelName: 'Comment',
+				tableName: 'comments',
+				charset: 'utf8mb4',
+				collate: 'utf8mb4_general_ci', // 이모티콘 저장
+				sequelize,
+			},
+		);
+	}
+
+	static associate(db) {
+		// 하나의 포스트는 여러개의 댓글을 가진다
+		db.Comment.belongsTo(db.Post);
+		// 하나의 유저는 여러개의 댓글을 가진다.
+		db.Comment.belongsTo(db.User);
+	}
+};
