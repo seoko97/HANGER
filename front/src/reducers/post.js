8a11dc31383fc7d9e5ee7cfe42cf482f6f86a320
seokho10007 <sukho1007@naver.com>
2021-01-15 04:17:41 +0900

    [feat] 게시글 작성 기능 추가

--- front/src/reducers/post.js
+++ front/src/reducers/post.js
@@ -5,13 +5,21 @@ export const ADD_COMMENT_REQUEST = 'ADD_COMMENT_REQUEST';
 export const ADD_COMMENT_SUCCESS = 'ADD_COMMENT_SUCCESS';
 export const ADD_COMMENT_FAILURE = 'ADD_COMMENT_FAILURE';
 
-export const LOAD_POSTS_REQUEST = 'LOAD_POSTS_REQUEST';
-export const LOAD_POSTS_SUCCESS = 'LOAD_POSTS_SUCCESS';
-export const LOAD_POSTS_FAILURE = 'LOAD_POSTS_FAILURE';
+export const LOAD_MAIN_POSTS_REQUEST = 'LOAD_MAIN_POSTS_REQUEST';
+export const LOAD_MAIN_POSTS_SUCCESS = 'LOAD_MAIN_POSTS_SUCCESS';
+export const LOAD_MAIN_POSTS_FAILURE = 'LOAD_MAIN_POSTS_FAILURE';
+
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
 
 export const initialState = {
 	mainPosts: [],
 	hasMorePosts: true,
+
+	addPostLoading: false, // 포스트 생성
+	addPostDone: false,
+	addPostError: null,
 
 	loadPostsLoading: false, // 포스트 불러오기
 	loadPostsDone: false,
@@ -35,17 +43,33 @@ const reducer = (state = initialState, action) =>
 	produce(state, (draft) => {
 		switch (action.type) {
 			// 포스트 불러오기
-			case LOAD_POSTS_REQUEST:
+			case ADD_POST_REQUEST:
+				draft.addPostLoading = true;
+				draft.addPostError = null;
+				draft.addPostDone = false;
+				break;
+			case ADD_POST_SUCCESS:
+				draft.addPostLoading = false;
+				draft.addPostDone = true;
+				draft.mainPosts.unshift(action.data);
+				break;
+			case ADD_POST_FAILURE:
+				draft.addPostLoading = false;
+				draft.addPostError = action.error;
+				break;
+
+			// 포스트 불러오기
+			case LOAD_MAIN_POSTS_REQUEST:
 				draft.loadPostsLoading = true;
 				draft.loadPostsError = null;
 				draft.loadPostsDone = false;
 				break;
-			case LOAD_POSTS_SUCCESS:
+			case LOAD_MAIN_POSTS_SUCCESS:
 				draft.loadPostsLoading = false;
 				draft.loadPostsDone = true;
 				draft.mainPosts = action.data.concat(draft.mainPosts);
 				break;
-			case LOAD_POSTS_FAILURE:
+			case LOAD_MAIN_POSTS_FAILURE:
 				draft.loadPostsLoading = false;
 				draft.loadPostsError = action.error;
 				break;
@@ -57,9 +81,7 @@ const reducer = (state = initialState, action) =>
 				draft.addCommentDone = false;
 				break;
 			case ADD_COMMENT_SUCCESS:
-				const post = draft.mainPosts.find(
-					(v) => v.id === action.data.postId,
-				);
+				const post = draft.mainPosts.find((v) => v.id === action.data.postId);
 				post.Comments.unshift(dummyComment);
 				draft.addCommentLoading = false;
 				draft.addCommentDone = true;
