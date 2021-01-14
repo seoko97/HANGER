14b80799831b84fcec439254818643712fe5fe81
seokho10007 <sukho1007@naver.com>
2021-01-15 04:23:59 +0900

    [feat] 서버 측 주소 변경

--- front/src/sagas/index.js
+++ front/src/sagas/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import userSaga from './user';
 import postSaga from './post';
 
-axios.defaults.baseURL = 'http://localhost:3060';
+axios.defaults.baseURL = 'http://localhost:3065';
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
