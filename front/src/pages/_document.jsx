68e5c41b95cc9b744b0807f7b8086c55d375821a
seokho10007 <sukho1007@naver.com>
2021-01-15 04:15:43 +0900

    [chore] 글자단 수정

--- front/src/pages/_document.jsx
+++ front/src/pages/_document.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -5,11 +6,11 @@ class MyDocument extends Document {
 	static async getInitialProps(ctx) {
 		const sheet = new ServerStyleSheet();
 		const originalRenderPage = ctx.renderPage;
+
 		try {
 			ctx.renderPage = () =>
 				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(<App {...props} />),
+					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
 				});
 			const initialProps = await Document.getInitialProps(ctx);
 			return {
