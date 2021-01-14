4577a35c9f6b56cdf3cc9308410eba5aa99fb605
seokho10007 <sukho1007@naver.com>
2021-01-15 04:22:13 +0900

    [feat] 서버사이드랜더링 구현

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,17 +1,16 @@
+import axios from 'axios';
 import { Fragment, useEffect } from 'react';
+import { END } from 'redux-saga';
 import { useDispatch, useSelector } from 'react-redux';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
-import { LOAD_POSTS_REQUEST } from '../reducers/post';
+import { LOAD_MAIN_POSTS_REQUEST } from '../reducers/post';
+import wrapper from '../store/configureStore';
+import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 
 const Home = () => {
-	const dispatch = useDispatch();
 	const { me } = useSelector((state) => state.user);
 	const { mainPosts } = useSelector((state) => state.post);
-
-	useEffect(() => {
-		dispatch({ type: LOAD_POSTS_REQUEST });
-	}, []);
 
 	return (
 		<Fragment>
@@ -22,5 +21,26 @@ const Home = () => {
 		</Fragment>
 	);
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+	console.log('getServerSideProps start');
+
+	const cookie = context.req?.headers.cookie;
+
+	context.req && cookie && (axios.defaults.headers.Cookie = cookie);
+
+	context.store.dispatch({
+		type: LOAD_MY_INFO_REQUEST,
+	});
+
+	context.store.dispatch({
+		type: LOAD_MAIN_POSTS_REQUEST,
+	});
+
+	context.store.dispatch(END);
+
+	// api 호출
+	await context.store.sagaTask.toPromise();
+});
 
 export default Home;
