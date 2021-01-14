00e0b24149ddb1a35afe198b143df17e64760216
seokho10007 <sukho1007@naver.com>
2021-01-15 04:23:21 +0900

    [chore] 주석제거

--- front/src/store/configureStore.js
+++ front/src/store/configureStore.js
@@ -16,6 +16,8 @@ const configureStore = () => {
 			: composeWithDevTools(applyMiddleware(...middleware));
 
 	const store = createStore(reducer, enhancer);
+
+	// withReduxSaga를 통한 서버사이드랜더링을 위해 설정 추가
 	store.sagaTask = sagaMiddleware.run(rootSaga);
 	return store;
 };
