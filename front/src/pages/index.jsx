311940fa5e56dd3e12e94feaf106b2f736981ac3
seokho10007 <sukho1007@naver.com>
2021-01-19 23:53:26 +0900

    [feat] index 페이지 모듈화

--- front/src/pages/index.jsx
+++ front/src/pages/index.jsx
@@ -1,30 +1,12 @@
 import axios from 'axios';
-import { Fragment, useEffect } from 'react';
 import { END } from 'redux-saga';
-import { useDispatch, useSelector } from 'react-redux';
-import PostCard from '../components/PostCard';
-import PostForm from '../components/PostForm';
 import { LOAD_MAIN_POSTS_REQUEST } from '../reducers/post';
 import wrapper from '../store/configureStore';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 
-const Home = () => {
-	const { me } = useSelector((state) => state.user);
-	const { mainPosts } = useSelector((state) => state.post);
-
-	return (
-		<Fragment>
-			{me && <PostForm />}
-			{mainPosts.map((c) => (
-				<PostCard key={c.id} post={c} />
-			))}
-		</Fragment>
-	);
-};
+export { default } from '../components/Home';
 
 export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
-	console.log('getServerSideProps start');
-
 	const cookie = context.req?.headers.cookie;
 
 	context.req && cookie && (axios.defaults.headers.Cookie = cookie);
@@ -42,5 +24,3 @@ export const getServerSideProps = wrapper.getServerSideProps(async (context) => 
 	// api 호출
 	await context.store.sagaTask.toPromise();
 });
-
-export default Home;
