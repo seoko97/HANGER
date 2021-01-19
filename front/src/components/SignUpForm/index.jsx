ceb41b42ab58fb152610048b3397b12655f0cda5
seokho10007 <sukho1007@naver.com>
2021-01-19 23:52:18 +0900

    [feat] 텍스트 검사 정규표현식 추가

--- front/src/components/SignUpForm/index.jsx
+++ front/src/components/SignUpForm/index.jsx
@@ -4,6 +4,7 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import Router from 'next/router';
 import { SIGN_UP_REQUEST } from '../../reducers/user';
 import useInput from '../../hooks/useInput';
+import useClick from '../../hooks/useClick';
 import Button from '../../components/UI/Button';
 import {
 	SignUpWrapper,
@@ -12,6 +13,7 @@ import {
 	SignUpInner,
 	SignUpMainContent,
 	CheckBox,
+	SignUpFormWrapper,
 } from './style';
 
 const SignUpFrom = () => {
@@ -19,12 +21,12 @@ const SignUpFrom = () => {
 	const [lastName, setLastName] = useInput('');
 	const [userId, setUserId] = useInput('');
 	const [nickname, setNickname] = useInput('');
-	const [birth, setBirth] = useInput('0000-00-00');
+	const [birth, setBirth] = useInput('');
 	const [gender, setGender] = useInput('');
+	const [term, setTerm] = useClick(false);
 
 	const [password, setPassword] = useState('');
 	const [passwordError, setPasswordError] = useState(false);
-	const [term, setTerm] = useState(false);
 	const [termError, setTermError] = useState(false);
 	const [visible, setVisible] = useState(false);
 	const [type, setType] = useState('password');
@@ -78,11 +80,6 @@ const SignUpFrom = () => {
 		[password, passwordError],
 	);
 
-	const useChecked = useCallback((e) => {
-		setTerm(e.target.checked);
-		e.target.checked && setTermError(false);
-	}, []);
-
 	const onChangeType = useCallback(() => {
 		setVisible(!visible);
 		type == 'password' ? setType('text') : setType('password');
@@ -90,142 +87,156 @@ const SignUpFrom = () => {
 
 	return (
 		<Fragment>
-			<SignUpWrapper>
-				<SignUpInner>
-					<SignUpHeader>
-						<div>가입하기</div>
-						<div>빠르고 간단합니다.</div>
-					</SignUpHeader>
-					<SignUpMainContent onSubmit={onSignUp}>
-						<InputBox>
-							<input
-								placeholder="성(姓)"
-								type="text"
-								value={firstName}
-								onChange={setFirstName}
-								required
-							/>
-							<input
-								placeholder="이름(성은 제외)"
-								type="text"
-								value={lastName}
-								onChange={setLastName}
-								required
-							/>
-						</InputBox>
-
-						<InputBox>
-							<input
-								placeholder="아이디"
-								type="text"
-								value={userId}
-								onChange={setUserId}
-								required
-							/>
-						</InputBox>
-
-						<InputBox>
-							<input
-								placeholder="비밀번호(4글자 이상)"
-								type={type}
-								value={password}
-								onChange={onChangePassword}
-								required
-							/>
-							<div onClick={onChangeType} title="비밀번호 확인">
-								{!visible ? (
-									<AiOutlineEye size={23} />
-								) : (
-									<AiOutlineEyeInvisible size={23} />
-								)}
-							</div>
-						</InputBox>
-						{passwordError && (
-							<div
-								style={{
-									color: 'red',
-									fontSize: '1.2rem',
-									marginBottom: '1.2rem',
-								}}
-							>
-								비밀번호는 4글자 이상이어야 합니다.
-							</div>
-						)}
-
-						<InputBox>
-							<input
-								placeholder="닉네임"
-								type="text"
-								value={nickname}
-								onChange={setNickname}
-								required
-							/>
-						</InputBox>
-
-						<div>
-							<span>생일</span>
-						</div>
-						<InputBox>
-							<input
-								type="date"
-								name="date"
-								value={birth}
-								onChange={setBirth}
-								required
-							/>
-						</InputBox>
-
-						<div>
-							<span>성별</span>
-						</div>
-						<div>
-							<CheckBox>
-								<label htmlFor="male">남성</label>
+			<SignUpFormWrapper>
+				<SignUpWrapper>
+					<SignUpInner>
+						<SignUpHeader>
+							<div>가입하기</div>
+							<div>빠르고 간단합니다.</div>
+						</SignUpHeader>
+						<SignUpMainContent onSubmit={onSignUp}>
+							<InputBox>
 								<input
-									type="radio"
-									name="gender"
-									value="male"
+									placeholder="성(姓)"
+									type="text"
+									value={firstName}
+									onChange={setFirstName}
 									required
-									onClick={setGender}
 								/>
-							</CheckBox>
-
-							<CheckBox>
-								<label htmlFor="female">여성</label>
 								<input
-									type="radio"
-									name="gender"
-									value="female"
+									placeholder="이름(성은 제외)"
+									type="text"
+									value={lastName}
+									onChange={setLastName}
 									required
-									onClick={setGender}
 								/>
-							</CheckBox>
-						</div>
+							</InputBox>
 
-						<div>
-							<input
-								name="term"
-								type="checkbox"
-								checked={term}
-								onChange={useChecked}
-							/>
-							<label htmlFor="term">동의함</label>
-						</div>
-						{termError && (
-							<div
-								style={{
-									color: 'red',
-									fontSize: '1.2rem',
-									marginBottom: '1.2rem',
-								}}
-							>
-								약관에 동의하셔야 합니다.
+							<InputBox>
+								<input
+									placeholder="아이디(한글x)"
+									type="text"
+									value={userId}
+									onChange={setUserId}
+									onKeyUp={(e) =>
+										(e.target.value = e.target.value.replace(
+											/[^a-zA-Z-_0-9]/g,
+											'',
+										))
+									}
+									required
+								/>
+							</InputBox>
+
+							<InputBox>
+								<input
+									placeholder="비밀번호(4글자 이상)"
+									type={type}
+									value={password}
+									onChange={onChangePassword}
+									required
+								/>
+								<div onClick={onChangeType} title="비밀번호 확인">
+									{!visible ? (
+										<AiOutlineEye size={23} />
+									) : (
+										<AiOutlineEyeInvisible size={23} />
+									)}
+								</div>
+							</InputBox>
+							{passwordError && (
+								<div
+									style={{
+										color: 'red',
+										fontSize: '1.2rem',
+										marginBottom: '1.2rem',
+									}}
+								>
+									비밀번호는 4글자 이상이어야 합니다.
+								</div>
+							)}
+
+							<InputBox>
+								<input
+									placeholder="닉네임"
+									type="text"
+									value={nickname}
+									onChange={setNickname}
+									onKeyUp={(e) =>
+										(e.target.value = e.target.value.replace(
+											/[^a-zA-Z-_0-9]/g,
+											'',
+										))
+									}
+									required
+								/>
+							</InputBox>
+
+							<div>
+								<span>생일</span>
 							</div>
-						)}
-						<Button type="submit">가입하기</Button>
-					</SignUpMainContent>
-				</SignUpInner>
-			</SignUpWrapper>
+							<InputBox>
+								<input
+									type="date"
+									name="date"
+									value={birth}
+									onChange={setBirth}
+									required
+								/>
+							</InputBox>
+
+							<div>
+								<span>성별</span>
+							</div>
+							<div>
+								<CheckBox>
+									<label htmlFor="male">남성</label>
+									<input
+										type="radio"
+										name="gender"
+										value="male"
+										required
+										onClick={setGender}
+									/>
+								</CheckBox>
+
+								<CheckBox>
+									<label htmlFor="female">여성</label>
+									<input
+										type="radio"
+										name="gender"
+										value="female"
+										required
+										onClick={setGender}
+									/>
+								</CheckBox>
+							</div>
+
+							<div onClick={setTerm}>
+								<input
+									name="term"
+									type="checkbox"
+									checked={term}
+									onChange={setTerm}
+								/>
+								<label htmlFor="term">동의함</label>
+							</div>
+							{termError && (
+								<div
+									style={{
+										color: 'red',
+										fontSize: '1.2rem',
+										marginBottom: '1.2rem',
+									}}
+								>
+									약관에 동의하셔야 합니다.
+								</div>
+							)}
+							<Button type="submit">가입하기</Button>
+						</SignUpMainContent>
+					</SignUpInner>
+				</SignUpWrapper>
+			</SignUpFormWrapper>
 		</Fragment>
 	);
 };
