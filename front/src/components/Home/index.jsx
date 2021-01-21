3b5076457a9d28786f7e3916b7586424713f42d6
seokho10007 <sukho1007@naver.com>
2021-01-21 23:56:43 +0900

    [Feat] window 사이즈에 따른 여백 변경

--- front/src/components/Home/index.jsx
+++ front/src/components/Home/index.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import useWindowSize from '../../hooks/useWindowSize';
 import NoticeForm from '../NoticeForm';
 import PostCard from '../PostCard';
 import PostForm from '../PostForm';
-import { LeftForm, RightForm } from './style';
+import { HomeWrapper, LeftForm, RightForm } from './style';
 
 const Home = () => {
 	const { me } = useSelector((state) => state.user);
 	const { mainPosts } = useSelector((state) => state.post);
+	const componentRef = useRef();
+	const homeRef = useRef();
+	const { width: windowWidth } = useWindowSize();
+	const [rightRef, setRigthRef] = useState(0);
+
+	useEffect(() => {
+		setRigthRef(
+			(windowWidth - homeRef.current.offsetWidth) / 2 + componentRef.current.offsetWidth + 22,
+		);
+	}, [windowWidth, homeRef?.current, componentRef?.current]);
 
 	return (
 		<>
-			<LeftForm>
-				{me && <PostForm />}
-				{mainPosts.map((c) => (
-					<PostCard key={c.id} post={c} />
-				))}
-			</LeftForm>
-			<RightForm>
-				<NoticeForm />
-			</RightForm>
+			<HomeWrapper ref={homeRef}>
+				<LeftForm ref={componentRef}>
+					{me && <PostForm />}
+					{mainPosts.map((c) => (
+						<PostCard key={c.id} post={c} />
+					))}
+				</LeftForm>
+
+				<RightForm style={{ left: `${rightRef}px` }}>
+					<NoticeForm />
+				</RightForm>
+			</HomeWrapper>
 		</>
 	);
 };
