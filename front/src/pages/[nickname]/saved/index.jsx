25334229b996629c3cc1002d9fc7982184f9f771
seokho10007 <sukho1007@naver.com>
2021-01-26 05:24:59 +0900

    [Feat] Pages Profile SSR 구성

---
+++ front/src/pages/[nickname]/saved/index.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { END } from 'redux-saga';
+import wrapper from '../../../store/configureStore';
+import { LOAD_MY_INFO_REQUEST, LOAD_USER_INFO_REQUEST } from '../../../reducers/user';
+import { LOAD_USER_SAVE_POSTS_REQUEST } from '../../../reducers/post';
+
+export { default } from '../../../components/Pages/Profile/Saved';
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+	const cookie = context.req?.headers.cookie;
+
+	context.req && cookie && (axios.defaults.headers.Cookie = cookie);
+
+	context.store.dispatch({
+		type: LOAD_MY_INFO_REQUEST,
+	});
+
+	context.store.dispatch({
+		type: LOAD_USER_INFO_REQUEST,
+		data: { nickname: context.query.nickname },
+	});
+
+	context.store.dispatch({
+		type: LOAD_USER_SAVE_POSTS_REQUEST,
+		data: { nickname: context.query.nickname },
+	});
+
+	context.store.dispatch(END);
+
+	// api 호출
+	await context.store.sagaTask.toPromise();
+});
