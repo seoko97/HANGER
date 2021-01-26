6c89303f1feefe6333cf348e9a144c8acdcdac6f
seokho10007 <sukho1007@naver.com>
2021-01-27 02:24:13 +0900

    [Style] 반응형 레이아웃 변경

--- front/src/components/UserProfileForm/index.jsx
+++ front/src/components/UserProfileForm/index.jsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
-import Avatar from '../UI/Avatar';
 import { BsFilePost, BsBookmark } from 'react-icons/bs';
 import { AiOutlineHeart } from 'react-icons/ai';
 import Link from 'next/link';
@@ -18,13 +17,17 @@ import {
 	UlListWrapper,
 	UserInfoWrapper,
 	AvatarWrapper,
+	MobileHeader,
 } from './style';
+import Avatar from '../UI/Avatar';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const UserProfileForm = ({ children, postType }) => {
 	const { userInfo, me } = useSelector((state) => state.user);
 	const [postMenu, setPostMenu] = useState(postType === 'mainPost' ? true : false);
 	const [saveMenu, setSaveMenu] = useState(postType === 'saved' ? true : false);
 	const [likeMenu, setLikeMenu] = useState(postType === 'liked' ? true : false);
+	const { width: windowWidth } = useWindowSize();
 
 	const onClickMenu = useCallback(
 		(fstHan, twoHan, thrHan) => () => {
@@ -35,43 +38,75 @@ const UserProfileForm = ({ children, postType }) => {
 		[],
 	);
 
+	console.log(windowWidth);
+
 	return (
 		<>
 			{userInfo && (
 				<ProfileWrapper>
 					<ProfileHeader>
-						<AvatarWrapper>
-							<Avatar size={150} borderGradient={true}>
-								프로필이미지
-							</Avatar>
-						</AvatarWrapper>
+						{windowWidth >= 480 ? (
+							<>
+								<AvatarWrapper>
+									<Avatar size={150} borderGradient={true}>
+										프로필이미지
+									</Avatar>
+								</AvatarWrapper>
+								<UserInfoWrapper>
+									<NicknameWrapper>
+										<h2>{userInfo.nickname}</h2>
+										{me?.id === userInfo.id ? (
+											<ChangeProfile>프로필 변경</ChangeProfile>
+										) : (
+											<></>
+										)}
+									</NicknameWrapper>
 
-						<UserInfoWrapper>
-							<NicknameWrapper>
-								<h2>{userInfo.nickname}</h2>
-								{me?.id === userInfo.id ? (
-									<ChangeProfile>프로필 변경</ChangeProfile>
-								) : (
-									<></>
-								)}
-							</NicknameWrapper>
-							<UlListWrapper>
-								{/* 나중에 Link 처리 */}
-								<li>
-									팔로잉 <span>{userInfo.Followings}</span>
-								</li>
-								<li>
-									팔로워 <span>{userInfo.Followers}</span>
-								</li>
-								<li>
-									게시글 <span>{userInfo.Posts}</span>
-								</li>
-							</UlListWrapper>
-							<NameAndIntroWrapper>
-								<h1>{userInfo.firstName + userInfo.lastName}</h1>
-								{userInfo.introduction && <span>{userInfo.introduction}</span>}
-							</NameAndIntroWrapper>
-						</UserInfoWrapper>
+									<UlListWrapper>
+										{/* 나중에 Link 처리 */}
+										<li>
+											팔로잉 <span>{userInfo.Followings}</span>
+										</li>
+										<li>
+											팔로워 <span>{userInfo.Followers}</span>
+										</li>
+										<li>
+											게시글 <span>{userInfo.Posts}</span>
+										</li>
+									</UlListWrapper>
+
+									<NameAndIntroWrapper>
+										<h1>{userInfo.firstName + userInfo.lastName}</h1>
+										{userInfo.introduction && (
+											<span>{userInfo.introduction}</span>
+										)}
+									</NameAndIntroWrapper>
+								</UserInfoWrapper>
+							</>
+						) : (
+							<>
+								<MobileHeader>
+									<AvatarWrapper>
+										<Avatar size={150} borderGradient={true}>
+											프로필이미지
+										</Avatar>
+									</AvatarWrapper>
+									<NicknameWrapper>
+										<h2>{userInfo.nickname}</h2>
+										{me?.id === userInfo.id ? (
+											// 링크로 감싸기
+											<ChangeProfile>프로필 변경</ChangeProfile>
+										) : (
+											<></>
+										)}
+									</NicknameWrapper>
+								</MobileHeader>
+								<NameAndIntroWrapper>
+									<h1>{userInfo.firstName + userInfo.lastName}</h1>
+									{userInfo.introduction && <span>{userInfo.introduction}</span>}
+								</NameAndIntroWrapper>
+							</>
+						)}
 					</ProfileHeader>
 					<MainContentWrapper>
 						<Divtag>
@@ -80,7 +115,7 @@ const UserProfileForm = ({ children, postType }) => {
 									className={postMenu && 'click_menu'}
 									onClick={onClickMenu(setPostMenu, setSaveMenu, setLikeMenu)}
 								>
-									<BsFilePost />
+									<BsFilePost size={15} />
 									게시글
 								</NavMevuwrapper>
 							</Link>
@@ -90,7 +125,7 @@ const UserProfileForm = ({ children, postType }) => {
 										className={saveMenu && 'click_menu'}
 										onClick={onClickMenu(setSaveMenu, setPostMenu, setLikeMenu)}
 									>
-										<BsBookmark />
+										<BsBookmark size={15} />
 										저장됨
 									</NavMevuwrapper>
 								</Link>
@@ -100,7 +135,7 @@ const UserProfileForm = ({ children, postType }) => {
 									className={likeMenu && 'click_menu'}
 									onClick={onClickMenu(setLikeMenu, setPostMenu, setSaveMenu)}
 								>
-									<AiOutlineHeart />
+									<AiOutlineHeart size={15} />
 									좋아요
 								</NavMevuwrapper>
 							</Link>
