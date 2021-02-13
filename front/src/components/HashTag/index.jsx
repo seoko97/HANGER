80e307fd1cc6c8db4df355242da8db00a504ef9b
seokho10007 <sukho1007@naver.com>
2021-02-14 02:55:02 +0900

    [Feat] Link 추가

--- front/src/components/HashTag/index.jsx
+++ front/src/components/HashTag/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 
-const HashTag = ({ content }) => (
-	<>
-		{content.split(/(#[^\s#]+)/g).map((v, i) => {
-			if (v.match(/(#[^\s#]+)/)) {
-				return (
-					<Link href={`/`} key={i}>
-						<a>{v}</a>
-					</Link>
-				);
-			}
-			return v;
-		})}
-	</>
-);
+const HashTag = ({ content }) => {
+	return (
+		<>
+			{content.split(/(#[^\s#]+)/g).map((v, i) => {
+				if (v.match(/(#[^\s#]+)/)) {
+					return (
+						<Link href={`/tag/${encodeURIComponent(v.slice(1))}`} key={i}>
+							<a>{v}</a>
+						</Link>
+					);
+				}
+				return v;
+			})}
+		</>
+	);
+};
 
 export default HashTag;
