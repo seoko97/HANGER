a32c7abda89c5a7aaf1fc2ebdcf4a112f0bc2ce7
seokho10007 <sukho1007@naver.com>
2021-02-17 04:10:12 +0900

    [Feat] 환경변수 변경

--- back/models/index.js
+++ back/models/index.js
@@ -7,8 +7,10 @@ const hashtag = require('./hashtag');
 const notice = require('./notice');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.js')[env];
+const config = require('../config/config')[env];
 const db = {};
+
+console.log(config);
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
