a306774d353674009f3871a11ad73a6a38b05824
seokho10007 <sukho1007@naver.com>
2021-02-16 23:02:48 +0900

    [Feat] search reducer 추가

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,6 +2,7 @@ import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
 import user from './user';
 import post from './post';
+import search from './search';
 
 const rootReducer = (state, action) => {
 	switch (action.type) {
@@ -12,6 +13,7 @@ const rootReducer = (state, action) => {
 			const combineReducer = combineReducers({
 				user,
 				post,
+				search,
 			});
 			return combineReducer(state, action);
 		}
