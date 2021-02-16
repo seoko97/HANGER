92c3a6dcbcdbbc94f2183a06562a21d25e8d1603
seokho10007 <sukho1007@naver.com>
2021-02-16 23:10:32 +0900

    [Feat] 폴더 구조 변경

---
+++ front/src/components/Pages/SignInForm/index.jsx
@@ -0,0 +1,105 @@
+import React, { Fragment, useCallback, useEffect } from 'react';
+import Link from 'next/link';
+import router from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../../UI/Loader';
+import Button from '../../UI/Button';
+import useInput from '../../../hooks/useInput';
+import { SIGN_IN_REQUEST } from '../../../reducers/user';
+import {
+	GlayLine,
+	InputWrapper,
+	SignInFormWrapper,
+	SignInInner,
+	SignInWrapper,
+	SignupLink,
+	LoadInner,
+} from './style';
+
+const SignInForm = () => {
+	const [userId, setUserId] = useInput('');
+	const [password, setPassword] = useInput('');
+
+	const { me, signInDone, signInError, signInLoading } = useSelector((state) => state.user);
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (me) {
+			router.replace('/');
+		}
+	}, [me]);
+
+	useEffect(() => {
+		if (signInDone) {
+			router.replace('/');
+		}
+	}, [signInDone]);
+
+	useEffect(() => {
+		if (signInError) {
+			if (signInError !== null) alert(signInError);
+		}
+	}, [signInError]);
+
+	const onSignIn = useCallback(
+		(e) => {
+			e.preventDefault();
+
+			dispatch({
+				type: SIGN_IN_REQUEST,
+				data: {
+					userId,
+					password,
+				},
+			});
+		},
+		[userId, password],
+	);
+	return (
+		!me && (
+			<Fragment>
+				<SignInFormWrapper>
+					<SignInWrapper>
+						<SignInInner onSubmit={onSignIn}>
+							<h1>로그인</h1>
+							<div></div>
+							<InputWrapper>
+								<input
+									placeholder="아이디"
+									name="id"
+									value={userId}
+									onChange={setUserId}
+								/>
+							</InputWrapper>
+							<InputWrapper>
+								<input
+									placeholder="비밀번호"
+									name="password"
+									value={password}
+									onChange={setPassword}
+									type="password"
+								/>
+							</InputWrapper>
+							<Button htmlType="submit">
+								{signInLoading ? (
+									<LoadInner>
+										<Loader type="spin" color="#ccc" size={20} />
+									</LoadInner>
+								) : (
+									'로그인'
+								)}
+							</Button>
+							<GlayLine />
+							<SignupLink>
+								<Link href="/signup">새 계정 만들기</Link>
+							</SignupLink>
+						</SignInInner>
+					</SignInWrapper>
+				</SignInFormWrapper>
+			</Fragment>
+		)
+	);
+};
+
+export default React.memo(SignInForm);
---
+++ front/src/components/Pages/SignUpForm/style.js
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+import Card from '../../UI/Card';
+
+export const SignUpFormWrapper = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: -20px;
+
+	@media (max-width: ${({ theme }) => theme.deviceSizes.MOBILE}) {
+		margin-top: 0;
+	}
+`;
+
+export const SignUpWrapper = styled(Card)`
+	width: 100%;
+	max-width: 600px;
+	display: flex;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1), 0 8px 16px rgba(0, 0, 0, 0.1);
+`;
+
+export const SignUpInner = styled.div`
+	width: 100%;
+`;
+export const SignUpHeader = styled.div`
+	width: 100%;
+	padding: 2em 0 1em 2em;
+
+	border-bottom: 1px solid #ccc;
+	& > div:first-child {
+		font-size: 2.8rem;
+		font-weight: 600;
+	}
+	& > div:last-child {
+		font-size: 1.5rem;
+		color: #606770;
+		margin: 1rem 0;
+	}
+`;
+
+export const SignUpMainContent = styled.form`
+	width: 100%;
+	padding: 2em;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	& > div {
+		width: 100%;
+		display: flex;
+		& > span {
+			margin-bottom: 0.5rem;
+			font-size: 1.4rem;
+		}
+		& > label {
+			margin-left: 0.5rem;
+			font-size: 1.4rem;
+		}
+		& > input {
+			margin-bottom: 3rem;
+		}
+	}
+	& > button {
+		width: 25%;
+		min-width: 100px;
+	}
+`;
+
+export const InputBox = styled.div`
+	position: relative;
+
+	& > input {
+		width: 100%;
+		padding: 0.8rem 1rem;
+		font-size: 1.4rem;
+		border-radius: 5px;
+		border: 1px solid #ccd0d5;
+		&:nth-child(2) {
+			margin-left: 10px;
+		}
+		background-color: rgb(245, 246, 247);
+		&[type='date'] {
+			background-color: #fff;
+		}
+	}
+	& > div {
+		position: absolute;
+		top: 5px;
+		right: 1.2rem;
+		border-radius: 5px;
+
+		&:hover,
+		&:active {
+			background-color: grey;
+		}
+	}
+`;
+
+export const CheckBox = styled.div`
+	width: 100%;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	margin-bottom: 3rem;
+	border: 1px solid #ccd0d5;
+	&:nth-child(2) {
+		margin-left: 10px;
+	}
+	display: flex;
+	padding: 0.8rem 1rem;
+
+	justify-content: space-between;
+
+	& > label {
+		width: 100%;
+	}
+`;
