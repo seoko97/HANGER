360326f04a69c6a92b0dffd2912ce40cb37acd74
seokho10007 <sukho1007@naver.com>
2021-02-16 22:47:05 +0900

    [Feat] singlePost action 추가

--- front/src/components/Pages/Post/index.jsx
+++ front/src/components/Pages/Post/index.jsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Helmet from 'react-helmet';
 import { useSelector } from 'react-redux';
+import Router from 'next/router';
 import styled from 'styled-components';
 import PostCard from '../../PostCard';
 
-const SinglePostWrapper = styled.div`
-	position: relative;
-	top: -10px;
+const TagPageHeader = styled.div`
+	width: 100%;
+	height: 100px;
 	display: flex;
 	align-items: center;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	left: 0;
+	top: 70px;
+	background-color: #fff;
+	font-size: 1.6rem;
+	font-weight: 400;
+	margin-bottom: 1rem;
+	padding: 1rem;
+	border-bottom: 1px solid #e8e8e8;
+`;
+
+const HeaderInner = styled.div`
+	width: 40%;
+	min-width: 700px;
+	& > div:first-child {
+		font-size: 2.3rem;
+		margin-bottom: 1.3rem;
+		& > span {
+			font-weight: 300;
+		}
+	}
+	& span {
+		font-weight: 600;
+	}
+	@media (max-width: ${({ theme }) => theme.deviceSizes.TABLET}) {
+		min-width: 500px;
+	}
+
+	@media (max-width: ${({ theme }) => theme.deviceSizes.MOBILE}) {
+		width: 100%;
+		min-width: 0;
+	}
+`;
+
+const SinglePostWrapper = styled.div`
+	display: flex;
+	margin-top: 90px;
 	justify-content: center;
 	width: 100%;
+	@media (max-width: ${({ theme }) => theme.deviceSizes.MOBILE}) {
+		margin-top: 100px;
+	}
 `;
 
 const SinglePostInner = styled.div`
@@ -20,16 +65,56 @@ const SinglePostInner = styled.div`
 `;
 
 const SinglePost = () => {
-	const { singlePost } = useSelector((state) => state.post);
+	const { mainPosts } = useSelector((state) => state.post);
 
 	return (
-		<>
-			<SinglePostWrapper>
-				<SinglePostInner>
-					{singlePost[0] && <PostCard post={singlePost[0]} />}
-				</SinglePostInner>
-			</SinglePostWrapper>
-		</>
+		mainPosts[0] !== undefined && (
+			<>
+				<Helmet
+					title={
+						mainPosts[0]?.User.nickname
+							? `${mainPosts[0]?.User.nickname}님의 글`
+							: 'HANGER'
+					}
+					description={mainPosts[0]?.content}
+					meta={[
+						{
+							name: 'description',
+							content: mainPosts[0]?.content,
+						},
+						{
+							property: 'og:title',
+							content: `${mainPosts[0]?.User.nickname}님의 게시글`,
+						},
+						{
+							property: 'og:description',
+							content: mainPosts[0]?.content,
+						},
+						{
+							property: 'og:image',
+							content: mainPosts[0]?.Images[0]
+								? `http://localhost:3065/${mainPosts[0]?.Images[0].src}`
+								: '/logo.png',
+						},
+						{
+							property: 'og:url',
+							content: `http://localhost:3060/post/${mainPosts[0]?.id}`,
+						},
+					]}
+				/>
+				<SinglePostWrapper>
+					<TagPageHeader>
+						<HeaderInner>
+							<span>{mainPosts[0]?.User?.nickname} </span>
+							님의 게시글
+						</HeaderInner>
+					</TagPageHeader>
+					<SinglePostInner>
+						{mainPosts[0] && <PostCard post={mainPosts[0]} />}
+					</SinglePostInner>
+				</SinglePostWrapper>
+			</>
+		)
 	);
 };
 
