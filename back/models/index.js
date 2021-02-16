188307d23831ff1e384031aa03b5a435e127db64
seokho10007 <sukho1007@naver.com>
2021-02-17 03:37:10 +0900

    [Feat] 확장자 입력

--- back/models/index.js
+++ back/models/index.js
@@ -7,7 +7,7 @@ const hashtag = require('./hashtag');
 const notice = require('./notice');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
