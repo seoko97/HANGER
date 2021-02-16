d34d8e1d5ef9934384f06b1d54c510d003bc5828
seokho10007 <sukho1007@naver.com>
2021-02-16 23:07:17 +0900

    [Feat] userInfo action 추가

--- front/src/reducers/user.js
+++ front/src/reducers/user.js
@@ -20,9 +20,55 @@ export const SIGN_OUT_REQUEST = 'SIGN_OUT_REQUEST';
 export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS';
 export const SIGN_OUT_FAILURE = 'SIGN_OUT_FAILURE';
 
+export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+
+export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+
+export const USER_FOLLOWERS_REQUEST = 'USER_FOLLOWERS_REQUEST';
+export const USER_FOLLOWERS_SUCCESS = 'USER_FOLLOWERS_SUCCESS';
+export const USER_FOLLOWERS_FAILURE = 'USER_FOLLOWERS_FAILURE';
+
+export const USER_FOLLOWINGS_REQUEST = 'USER_FOLLOWINGS_REQUEST';
+export const USER_FOLLOWINGS_SUCCESS = 'USER_FOLLOWINGS_SUCCESS';
+export const USER_FOLLOWINGS_FAILURE = 'USER_FOLLOWINGS_FAILURE';
+
+export const CHANGE_USER_INFO_REQUEST = 'CHANGE_USER_INFO_REQUEST';
+export const CHANGE_USER_INFO_SUCCESS = 'CHANGE_USER_INFO_SUCCESS';
+export const CHANGE_USER_INFO_FAILURE = 'CHANGE_USER_INFO_FAILURE';
+
+export const CHANGE_USER_PROFILE_IMAGE_REQUEST = 'CHANGE_USER_PROFILE_IMAGE_REQUEST';
+export const CHANGE_USER_PROFILE_IMAGE_SUCCESS = 'CHANGE_USER_PROFILE_IMAGE_SUCCESS';
+export const CHANGE_USER_PROFILE_IMAGE_FAILURE = 'CHANGE_USER_PROFILE_IMAGE_FAILURE';
+
+export const LOAD_USER_NOTICE_REQUEST = 'LOAD_USER_NOTICE_REQUEST';
+export const LOAD_USER_NOTICE_SUCCESS = 'LOAD_USER_NOTICE_SUCCESS';
+export const LOAD_USER_NOTICE_FAILURE = 'LOAD_USER_NOTICE_FAILURE';
+
+export const LOAD_USER_FOLLOWNOTICE_REQUEST = 'LOAD_USER_FOLLOWNOTICE_REQUEST';
+export const LOAD_USER_FOLLOWNOTICE_SUCCESS = 'LOAD_USER_FOLLOWNOTICE_SUCCESS';
+export const LOAD_USER_FOLLOWNOTICE_FAILURE = 'LOAD_USER_FOLLOWNOTICE_FAILURE';
+
+export const LOAD_NOTICE_LIST_REQUEST = 'LOAD_NOTICE_LIST_REQUEST';
+export const LOAD_NOTICE_LIST_SUCCESS = 'LOAD_NOTICE_LIST_SUCCESS';
+export const LOAD_NOTICE_LIST_FAILURE = 'LOAD_NOTICE_LIST_FAILURE';
+
 export const initialState = {
 	me: null,
 	userInfo: null,
+	userProfileImg: null,
+	userFollowers: [],
+	userFollowings: [],
+	userNotice: [],
+	userFollowNotice: [],
+	userMobileNotice: [],
+
+	hasMoreNotices: true,
+	hasMoreFollowNotices: true,
+	hasMoreMoblieNotices: true,
 
 	signUpLoading: false, // 회원가입
 	signUpDone: false,
@@ -43,6 +89,38 @@ export const initialState = {
 	loadUserInfoLoading: false, // 유저 정보 호출
 	loadUserInfoDone: false,
 	loadUserInfoError: null,
+
+	changeUserProfileImageLoading: false, // 유저 정보 호출
+	changeUserProfileImageDone: false,
+	changeUserProfileImageError: null,
+
+	followLoading: false, // 팔로우
+	followDone: false,
+	followError: null,
+
+	unFollowLoading: false, // 언팔로우
+	unFollowDone: false,
+	unFollowError: null,
+
+	userFollowersLoading: false, // 팔로워 목록
+	userFollowersDone: false,
+	userFollowersError: null,
+
+	userFollowingsLoading: false, // 팔로우 목록
+	userFollowingsDone: false,
+	userFollowingsError: null,
+
+	userNoticeLoading: false, // 알림 목록
+	userNoticeDone: false,
+	userNoticeError: null,
+
+	userFollowNoticeLoading: false, // 팔로우 알림
+	userFollowNoticeDone: false,
+	userFollowNoticeError: null,
+
+	userMobileNoticeLoading: false, // 팔로우 알림
+	userMobileNoticeDone: false,
+	userMobileNoticeError: null,
 };
 
 const reducer = (state = initialState, action) =>
@@ -96,24 +174,45 @@ const reducer = (state = initialState, action) =>
 				draft.signOutError = action.error;
 				break;
 
-			// 내정보 호출
+			// 프로필 이미지 변경
+			case CHANGE_USER_PROFILE_IMAGE_REQUEST:
+				draft.changeUserProfileImageLoading = true;
+				draft.changeUserProfileImageError = null;
+				draft.changeUserProfileImageDone = false;
+				break;
+			case CHANGE_USER_PROFILE_IMAGE_SUCCESS:
+				draft.userProfileImg = action.data;
+				draft.changeUserProfileImageLoading = false;
+				draft.changeUserProfileImageDone = true;
+				break;
+
+			case CHANGE_USER_PROFILE_IMAGE_FAILURE:
+				draft.changeUserProfileImageLoading = false;
+				draft.changeUserProfileImageError = action.error;
+				break;
+
+			// 내정보 호출, 유저 정보 변경
 			case LOAD_MY_INFO_REQUEST:
+			case CHANGE_USER_INFO_REQUEST:
 				draft.loadMyInfoLoading = true;
 				draft.loadMyInfoError = null;
 				draft.loadMyInfoDone = false;
 				break;
 			case LOAD_MY_INFO_SUCCESS:
+			case CHANGE_USER_INFO_SUCCESS:
 				draft.me = action.data;
 				draft.loadMyInfoLoading = false;
 				draft.loadMyInfoDone = true;
 				break;
 			case LOAD_MY_INFO_FAILURE:
+			case CHANGE_USER_INFO_FAILURE:
 				draft.loadMyInfoLoading = false;
 				draft.loadMyInfoError = action.error;
 				break;
 
 			// 내정보 호출
 			case LOAD_USER_INFO_REQUEST:
+				draft.userInfo = null;
 				draft.loadUserInfoLoading = true;
 				draft.loadUserInfoError = null;
 				draft.loadUserInfoDone = false;
@@ -126,6 +225,136 @@ const reducer = (state = initialState, action) =>
 			case LOAD_USER_INFO_FAILURE:
 				draft.loadUserInfoLoading = false;
 				draft.loadUserInfoError = action.error;
+				break;
+
+			// 팔로우
+			case FOLLOW_REQUEST:
+				draft.followLoading = true;
+				draft.followError = null;
+				draft.followDone = false;
+				break;
+			case FOLLOW_SUCCESS:
+				draft.me.Followings.push({ id: action.data.UserId });
+
+				if (draft.userInfo?.id === action.data.UserId) {
+					draft.userInfo.Followers = draft.userInfo.Followers + 1;
+				}
+				draft.followLoading = false;
+				draft.followDone = true;
+				break;
+
+			case FOLLOW_FAILURE:
+				draft.followLoading = false;
+				draft.followError = action.error;
+				break;
+
+			// 언팔로우
+			case UNFOLLOW_REQUEST:
+				draft.UnFollowLoading = true;
+				draft.UnFollowError = null;
+				draft.UnFollowDone = false;
+				break;
+			case UNFOLLOW_SUCCESS:
+				draft.me.Followings = draft.me.Followings.filter(
+					(v) => v.id !== action.data.UserId,
+				);
+
+				if (draft.userInfo?.id == action.data.UserId) {
+					draft.userInfo.Followers = draft.userInfo.Followers - 1;
+				}
+				draft.UnFollowLoading = false;
+				draft.UnFollowDone = true;
+				break;
+			case UNFOLLOW_FAILURE:
+				draft.UnFollowLoading = false;
+				draft.UnFollowError = action.error;
+				break;
+
+			// 팔로워 목록
+			case USER_FOLLOWERS_REQUEST:
+				draft.userFollowers = [];
+				draft.userFollowersLoading = true;
+				draft.userFollowersError = null;
+				draft.userFollowersDone = false;
+				break;
+			case USER_FOLLOWERS_SUCCESS:
+				draft.userFollowers = draft.userFollowers.concat(action.data);
+				draft.userFollowersLoading = false;
+				draft.userFollowersDone = true;
+				break;
+			case USER_FOLLOWERS_FAILURE:
+				draft.userFollowersLoading = false;
+				draft.userFollowersError = action.error;
+				break;
+
+			// 팔로잉 목록
+			case USER_FOLLOWINGS_REQUEST:
+				draft.userFollowings = [];
+				draft.userFollowingsLoading = true;
+				draft.userFollowingsError = null;
+				draft.userFollowingsDone = false;
+				break;
+			case USER_FOLLOWINGS_SUCCESS:
+				draft.userFollowings = draft.userFollowings.concat(action.data);
+				draft.userFollowingsLoading = false;
+				draft.userFollowingsDone = true;
+				break;
+			case USER_FOLLOWINGS_FAILURE:
+				draft.userFollowingsLoading = false;
+				draft.userFollowingsError = action.error;
+				break;
+
+			// 알림 목록
+			case LOAD_USER_NOTICE_REQUEST:
+				draft.userNoticeLoading = true;
+				draft.userNoticeError = null;
+				draft.userNoticeDone = false;
+				break;
+			case LOAD_USER_NOTICE_SUCCESS:
+				draft.userNotice = draft.userNotice.concat(action.data);
+				draft.userNoticeLoading = false;
+				draft.userNoticeDone = true;
+				draft.hasMoreNotices = action.data.length === 10;
+				break;
+			case LOAD_USER_NOTICE_FAILURE:
+				draft.userNoticeLoading = false;
+				draft.userNoticeError = action.error;
+				break;
+
+			// 알림 목록
+			case LOAD_USER_FOLLOWNOTICE_REQUEST:
+				draft.userFollowNotice = [];
+				draft.userFollowNoticeLoading = true;
+				draft.userFollowNoticeError = null;
+				draft.userFollowNoticeDone = false;
+				break;
+			case LOAD_USER_FOLLOWNOTICE_SUCCESS:
+				draft.userFollowNotice = draft.userFollowNotice.concat(action.data);
+				draft.userFollowNoticeLoading = false;
+				draft.userFollowNoticeDone = true;
+				draft.hasMoreFollowNotices = action.data.length === 10;
+
+				break;
+			case LOAD_USER_FOLLOWNOTICE_FAILURE:
+				draft.userFollowNoticeLoading = false;
+				draft.userFollowNoticeError = action.error;
+				break;
+
+			// 모바일 알림
+			case LOAD_NOTICE_LIST_REQUEST:
+				draft.userMobileNoticeLoading = true;
+				draft.userMobileNoticeError = null;
+				draft.userMobileNoticeDone = false;
+				break;
+			case LOAD_NOTICE_LIST_SUCCESS:
+				draft.userMobileNotice = draft.userMobileNotice.concat(action.data);
+				draft.userMobileNoticeLoading = false;
+				draft.userMobileNoticeDone = true;
+				draft.hasMoreMoblieNotices = action.data.length === 10;
+				break;
+			case LOAD_NOTICE_LIST_FAILURE:
+				draft.userMobileNoticeLoading = false;
+				draft.userMobileNoticeError = action.error;
 				break;
 		}
 	});
--- front/src/sagas/user.js
+++ front/src/sagas/user.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
-import { all, call, delay, fork, put, takeLatest } from 'redux-saga/effects';
+import { all, call, delay, fork, put, takeLatest, throttle } from 'redux-saga/effects';
 import {
+	CHANGE_USER_INFO_FAILURE,
+	CHANGE_USER_INFO_REQUEST,
+	CHANGE_USER_INFO_SUCCESS,
+	CHANGE_USER_PROFILE_IMAGE_FAILURE,
+	CHANGE_USER_PROFILE_IMAGE_REQUEST,
+	CHANGE_USER_PROFILE_IMAGE_SUCCESS,
 	LOAD_MY_INFO_FAILURE,
 	LOAD_MY_INFO_REQUEST,
 	LOAD_MY_INFO_SUCCESS,
 	LOAD_USER_INFO_FAILURE,
 	LOAD_USER_INFO_REQUEST,
 	LOAD_USER_INFO_SUCCESS,
+	LOAD_USER_NOTICE_FAILURE,
+	LOAD_USER_NOTICE_REQUEST,
+	LOAD_USER_NOTICE_SUCCESS,
+	LOAD_USER_FOLLOWNOTICE_FAILURE,
+	LOAD_USER_FOLLOWNOTICE_REQUEST,
+	LOAD_USER_FOLLOWNOTICE_SUCCESS,
 	SIGN_IN_FAILURE,
 	SIGN_IN_REQUEST,
 	SIGN_IN_SUCCESS,
@@ -16,6 +28,21 @@ import {
 	SIGN_UP_FAILURE,
 	SIGN_UP_REQUEST,
 	SIGN_UP_SUCCESS,
+	FOLLOW_FAILURE,
+	FOLLOW_REQUEST,
+	FOLLOW_SUCCESS,
+	UNFOLLOW_FAILURE,
+	UNFOLLOW_REQUEST,
+	UNFOLLOW_SUCCESS,
+	USER_FOLLOWERS_FAILURE,
+	USER_FOLLOWERS_REQUEST,
+	USER_FOLLOWERS_SUCCESS,
+	USER_FOLLOWINGS_FAILURE,
+	USER_FOLLOWINGS_REQUEST,
+	USER_FOLLOWINGS_SUCCESS,
+	LOAD_NOTICE_LIST_FAILURE,
+	LOAD_NOTICE_LIST_REQUEST,
+	LOAD_NOTICE_LIST_SUCCESS,
 } from '../reducers/user';
 
 function signUpAPI(data) {
@@ -30,10 +57,9 @@ function* signUp(action) {
 			type: SIGN_UP_SUCCESS,
 		});
 	} catch (err) {
-		console.error(err);
 		yield put({
 			type: SIGN_UP_FAILURE,
-			error: err.response.data,
+			error: err.response?.data ? err.response.data : err,
 		});
 	}
 }
@@ -51,10 +77,9 @@ function* signIn(action) {
 			data: result.data,
 		});
 	} catch (err) {
-		console.error(err);
 		yield put({
 			type: SIGN_IN_FAILURE,
-			error: err.response.data,
+			error: err.response?.data ? err.response.data : err,
 		});
 	}
 }
@@ -71,10 +96,9 @@ function* signOut() {
 			type: SIGN_OUT_SUCCESS,
 		});
 	} catch (err) {
-		console.error(err);
 		yield put({
 			type: SIGN_OUT_FAILURE,
-			error: err.response.data,
+			error: err.response?.data ? err.response.data : err,
 		});
 	}
 }
@@ -92,10 +116,9 @@ function* loadMyInfo() {
 			data: result.data,
 		});
 	} catch (err) {
-		console.error(err);
 		yield put({
 			type: LOAD_MY_INFO_FAILURE,
-			error: err.response.data,
+			error: err.response?.data ? err.response.data : err,
 		});
 	}
 }
@@ -113,13 +136,233 @@ function* loadUserInfo(action) {
 			data: result.data,
 		});
 	} catch (err) {
-		console.error(err);
 		yield put({
 			type: LOAD_USER_INFO_FAILURE,
-			error: err.response.data,
+			error: err.response?.data ? err.response.data : err,
 		});
 	}
 }
+
+function changeUserInfoAPI(data) {
+	return axios.patch(`/user/${data.oldNickname}/edit`, data);
+}
+
+function* changeUserInfo(action) {
+	try {
+		const result = yield call(changeUserInfoAPI, action.data);
+
+		yield put({
+			type: CHANGE_USER_INFO_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: CHANGE_USER_INFO_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function changeUserProfileImgAPI(data) {
+	return axios.patch(`/user/${data.get('nickname')}/profileimg`, data);
+}
+
+function* changeUserProfileImg(action) {
+	try {
+		const result = yield call(changeUserProfileImgAPI, action.data);
+
+		yield put({
+			type: CHANGE_USER_PROFILE_IMAGE_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: CHANGE_USER_PROFILE_IMAGE_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function followAPI(data) {
+	return axios.patch(`/user/${data}/follow`);
+}
+
+function* follow(action) {
+	try {
+		const result = yield call(followAPI, action.data);
+
+		yield put({
+			type: FOLLOW_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: FOLLOW_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function unFollowAPI(data) {
+	return axios.delete(`/user/${data}/follow`);
+}
+
+function* unFollow(action) {
+	try {
+		const result = yield call(unFollowAPI, action.data);
+
+		yield put({
+			type: UNFOLLOW_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: UNFOLLOW_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function userFollowersAPI(data) {
+	return axios.get(`/user/${data}/followers`);
+}
+
+function* userFollowers(action) {
+	try {
+		const result = yield call(userFollowersAPI, action.data);
+
+		yield put({
+			type: USER_FOLLOWERS_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: USER_FOLLOWERS_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function userFollowingsAPI(data) {
+	return axios.get(`/user/${data}/followings`);
+}
+
+function* userFollowings(action) {
+	try {
+		const result = yield call(userFollowingsAPI, action.data);
+
+		yield put({
+			type: USER_FOLLOWINGS_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: USER_FOLLOWINGS_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function loadUserNoticeAPI(lastId) {
+	return axios.post(`/user/notice?lastId=${lastId || 0}`);
+}
+
+function* loadUserNotice(action) {
+	try {
+		const result = yield call(loadUserNoticeAPI, action.lastId);
+
+		yield delay(800);
+
+		yield put({
+			type: LOAD_USER_NOTICE_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: LOAD_USER_NOTICE_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function loadUserFollowNoticeAPI(lastId) {
+	return axios.post(`/user/followNotice?lastId=${lastId || 0}`);
+}
+
+function* loadUserFollowNotice(action) {
+	try {
+		const result = yield call(loadUserFollowNoticeAPI, action.lastId);
+
+		yield put({
+			type: LOAD_USER_FOLLOWNOTICE_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: LOAD_USER_FOLLOWNOTICE_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function loadUserMobileNoticeAPI(lastId) {
+	return axios.post(`/user/moblieNotice?lastId=${lastId || 0}`);
+}
+
+function* loadUserMobileNotice(action) {
+	try {
+		const result = yield call(loadUserMobileNoticeAPI, action.lastId);
+
+		yield delay(1000);
+
+		yield put({
+			type: LOAD_NOTICE_LIST_SUCCESS,
+			data: result.data,
+		});
+	} catch (err) {
+		yield put({
+			type: LOAD_NOTICE_LIST_FAILURE,
+			error: err.response?.data ? err.response.data : err,
+		});
+	}
+}
+
+function* watchLoadUserMobileNotice() {
+	yield takeLatest(LOAD_NOTICE_LIST_REQUEST, loadUserMobileNotice);
+}
+
+function* watchLoadUserFollowNotice() {
+	yield takeLatest(LOAD_USER_FOLLOWNOTICE_REQUEST, loadUserFollowNotice);
+}
+
+function* watchLoadUserNotice() {
+	yield takeLatest(LOAD_USER_NOTICE_REQUEST, loadUserNotice);
+}
+
+function* watchUserFollowings() {
+	yield takeLatest(USER_FOLLOWINGS_REQUEST, userFollowings);
+}
+
+function* watchUserFollowers() {
+	yield takeLatest(USER_FOLLOWERS_REQUEST, userFollowers);
+}
+
+function* watchUnFollow() {
+	yield takeLatest(UNFOLLOW_REQUEST, unFollow);
+}
+
+function* watchFollow() {
+	yield takeLatest(FOLLOW_REQUEST, follow);
+}
+
+function* watchChangeUserProfileImg() {
+	yield takeLatest(CHANGE_USER_PROFILE_IMAGE_REQUEST, changeUserProfileImg);
+}
+
+function* watchChangeUserInfo() {
+	yield takeLatest(CHANGE_USER_INFO_REQUEST, changeUserInfo);
+}
+
 function* watchLoadUserInfo() {
 	yield takeLatest(LOAD_USER_INFO_REQUEST, loadUserInfo);
 }
@@ -147,5 +390,14 @@ export default function* userSaga() {
 		fork(watchSignIn),
 		fork(watchSignOut),
 		fork(watchLoadMyInfo),
+		fork(watchChangeUserInfo),
+		fork(watchChangeUserProfileImg),
+		fork(watchFollow),
+		fork(watchUnFollow),
+		fork(watchUserFollowers),
+		fork(watchUserFollowings),
+		fork(watchLoadUserNotice),
+		fork(watchLoadUserFollowNotice),
+		fork(watchLoadUserMobileNotice),
 	]);
 }
