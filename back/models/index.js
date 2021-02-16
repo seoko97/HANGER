274ef08a54a4eb90a5cdafd2dac538a28a922e40
seokho10007 <sukho1007@naver.com>
2021-02-17 04:17:43 +0900

    [Feat] 스펠링 오류 수정

--- back/models/index.js
+++ back/models/index.js
@@ -10,8 +10,6 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
-console.log(config);
-
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.User = user;
