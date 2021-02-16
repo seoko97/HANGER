2b36c03d395db4aab82393629a605c109913ce6b
seokho10007 <sukho1007@naver.com>
2021-02-16 23:03:07 +0900

    [Feat] tag, search saga 추가

--- front/src/sagas/index.js
+++ front/src/sagas/index.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 
 import userSaga from './user';
 import postSaga from './post';
+import searchSaga from './search';
+import tagSaga from './tag';
 
 axios.defaults.baseURL = 'http://localhost:3065';
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
-	yield all([fork(userSaga), fork(postSaga)]);
+	yield all([fork(userSaga), fork(postSaga), fork(searchSaga), fork(tagSaga)]);
 }
