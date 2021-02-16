f7411f3fbca888f738666e1cff27eb9f29a28634
seokho10007 <sukho1007@naver.com>
2021-02-16 23:00:03 +0900

    [Feat] SSR을 통한 로그인 유저 정보 호출

--- front/src/pages/signup/index.jsx
+++ front/src/pages/signup/index.jsx
@@ -1,1 +1,20 @@
-export { default } from '../../components/SignUpForm';
+import axios from 'axios';
+import { END } from 'redux-saga';
+import wrapper from '../../store/configureStore';
+import { LOAD_MY_INFO_REQUEST } from '../../reducers/user';
+
+export { default } from '../../components/Pages/SignUpForm';
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+	const cookie = context.req?.headers.cookie;
+
+	context.req && cookie && (axios.defaults.headers.Cookie = cookie);
+
+	context.store.dispatch({
+		type: LOAD_MY_INFO_REQUEST,
+	});
+
+	context.store.dispatch(END);
+
+	await context.store.sagaTask.toPromise();
+});
